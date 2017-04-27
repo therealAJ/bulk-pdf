d9677631e0965fc797e6d66d87208967dee31613
Alex Jordache <alexonej@gmail.com>
2017-04-27 00:15:48 -0700

    add argparse

--- pdf.py
+++ pdf.py
@@ -2,10 +2,19 @@ import re
 import sys
 import os
 import requests
+import argparse
 from urllib.request import urlopen
 
-# Our URL 
+parser = argparse.ArgumentParser()
+parser.add_argument("a")
+parser.add_argument('b', nargs='?')
+args = parser.parse_args()
+print(args)
+
+# Parse URL 
 url_link = str(sys.argv[1])
+# wget URL
+wget_link = str(sys.argv[2])
 
 website = urlopen(url_link)
 
@@ -14,7 +23,7 @@ html = website.read().decode('utf-8')
 
 #Find all local and non-hosted pdfs and download 
 links = re.findall('"(https?://\S*?.pdf)"', html)
-local_links = re.findall('"([^.=]*.pdf)', html)
+local_links = re.findall('"([^.\"=]*.pdf)', html)
 
 for link in links:
 	command = 'wget'
@@ -23,5 +32,5 @@ for link in links:
 
 for link in local_links:
 	command = 'wget'
-	path = str(sys.argv[2])
+	path = str(sys.argv[3])
 	os.system("%s %s%s -P %s" % (command, url_link, link, path))
